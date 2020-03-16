cd6a0f4742803fec664740274ec205635d5e2e82
Francois Allard <francois@breathelife.com>
2020-03-16 14:54:27 -0400

    Small change

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
-var assert           = require('assert');
-var normalizeNewline = require('normalize-newline');
-var read             = require('read-file-relative').readSync;
-var createReport     = require('./utils/create-report');
+var assert = require("assert");
+var normalizeNewline = require("normalize-newline");
+var read = require("read-file-relative").readSync;
+var createReport = require("./utils/create-report");
 
-it('Should produce report with colors', function () {
-    var report   = createReport(true);
-    var expected = JSON.parse(read('./data/report-with-colors.json'));
+it("Should produce report", function() {
+    var report = createReport(true);
+    var expected = JSON.parse(read("./data/report-with-colors.json"));
 
-    report   = normalizeNewline(report).trim();
+    report = normalizeNewline(report).trim();
     expected = normalizeNewline(expected).trim();
 
     assert.strictEqual(report, expected);
